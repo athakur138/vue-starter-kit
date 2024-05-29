768a689fd24d9abd045afaa3a9a89706dd0f7de2
GPSLT38 <athakur@covalience.com>
2024-05-30 01:09:38 +0530

    utility funtion for number to 2 decimal place

--- src/helpers/utility.ts
+++ src/helpers/utility.ts
@@ -5,4 +5,8 @@ export const uniqueArray = (): void => {
 export const reverseWords = (text: string): string => {
     return text.split(' ').map((word: string) => word.split('').reverse().join('')).join(' ');
   };
+
+  export const formatNumber = (num: number): string => {
+    return num.toFixed(2);
+};
   
