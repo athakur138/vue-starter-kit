e560b0315f9d2e2c673fa61f6cd7f0ea8c5b7847
GPSLT38 <athakur@covalience.com>
2024-05-30 01:07:08 +0530

    added common fun

--- src/helpers/utility.ts
+++ src/helpers/utility.ts
@@ -1,3 +1,8 @@
 export const uniqueArray = (): void => {
     // logic for the function
 }
+
+export const reverseWords = (text: string): string => {
+    return text.split(' ').map((word: string) => word.split('').reverse().join('')).join(' ');
+  };
+  
