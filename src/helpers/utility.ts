3de514f6e3cf50febe9ec9e6e67b31e0ce4bb33f
athakur138 <79656636+athakur138@users.noreply.github.com>
2024-05-30 01:30:01 +0530

    Feature/1 (#9)
    * added common fun
    
    * added heading
    
    * utility funtion for number to 2 decimal place
    
    * added a component

--- src/helpers/utility.ts
+++ src/helpers/utility.ts
@@ -1,3 +1,12 @@
 export const uniqueArray = (): void => {
     // logic for the function
 }
+
+export const reverseWords = (text: string): string => {
+    return text.split(' ').map((word: string) => word.split('').reverse().join('')).join(' ');
+  };
+
+  export const formatNumber = (num: number): string => {
+    return num.toFixed(2);
+};
+  
