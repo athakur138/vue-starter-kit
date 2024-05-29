e0d4e66f7f23f13432982450019f14bf1fa62486
athakur138 <79656636+athakur138@users.noreply.github.com>
2024-05-29 23:54:13 +0530

    Feature/1 test (#3)
    squashing

---
+++ src/views/Test/Count.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+// Create a reactive reference to store the count value
+const count = ref(0)
+
+</script>
+
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  /* font-weight: bold; */
+}
+</style>
