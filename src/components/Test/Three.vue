ca7b78037c10af91ac099bb62376e8570caddfc5
GPSLT38 <athakur@covalience.com>
2024-05-30 12:06:51 +0530

    rebase 1 commit

---
+++ src/components/Test/Three.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+    <h1>Hi</h1>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
